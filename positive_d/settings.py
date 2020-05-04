e963e4c624fbeb6874452cdb22e4a217a305b637
hkapoor99 <hrshtkpr4@gmail.com>
2020-05-04 11:36:11 +0530

    1st deployed version

--- positive_d/settings.py
+++ positive_d/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8b_xp@aqalskdx^oba==icccc$4ar8%7$&mn%v^xvbd=&+qb30'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
